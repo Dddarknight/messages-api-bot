bd1f7b89a9c402a73c50f56e48113c604496c8a4
Dddarknight <9801677@gmail.com>
2023-01-10 17:35:40 +0300

    change webhook

--- bot/bot.py
+++ bot/bot.py
@@ -53,8 +53,8 @@ def main():
     updater.dispatcher.add_error_handler(handle_error)
     updater.start_webhook(listen="0.0.0.0",
                           port=8443,
-                          url_path=API_TOKEN)
-    updater.bot.setWebhook(f'{TG_APP}/{API_TOKEN}')
+                          url_path=API_TOKEN,
+                          webhook_url=f'{TG_APP}/{API_TOKEN}')
     updater.idle()
 
 
